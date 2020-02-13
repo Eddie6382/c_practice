310f498bf1822648a68a66ecca3c07ee2cf4664d
Eddie6382 <b07901020@ntu.edu.tw>
2020-02-14 00:19:27 +0800

    123456

--- maximum_sub_matrices/source.cpp
+++ maximum_sub_matrices/source.cpp
@@ -6,28 +6,55 @@
 #include <vector>
 #include <limits>
 #include <algorithm>
+#include "../usage.h"
 
 using namespace std;
 
-void ShowMatrix(int* matrix, int m, int n, int sub_m1, int sub_m2, int sub_n1, int sub_n2)
+void ShowMatrix(const int* matrix, int m, int n, int sub_m1, int sub_m2, int sub_n1, int sub_n2)
 {
 	if (sub_m1 == 0 && sub_m2 == 0 && sub_n1 == 0 && sub_n2 == 0)
 		for (int i = 0; i < m; i++) 
         {
 			for (int j = 0; j < n; j++)
-				cout << *(matrix + i * n + j) << " ";
+				cout << setw(4) << *(matrix + i * n + j);
 			cout << endl;
 		}
 	else
 		for (int i = sub_m1; i <= sub_m2; i++)
 		{
 			for (int j = sub_n1; j <= sub_n2; j++)
-				cout << *(matrix + i * n + j) << " ";
+				cout << setw(4) << *(matrix + i * n + j);
 			cout << endl;
 		}
+    cout << endl;
 }
 
-int MaxSum(int* matrix, const int ROW, const int COL, int& sub_row1, int& sub_row2, int& sub_col1, int& sub_col2)
+int MaxSum_brute(const int* matrix, int ROW, int COL, int& sub_row1, int& sub_row2, int& sub_col1, int& sub_col2)
+{
+	int max = INT8_MIN;
+	for (int pivot_row = 0; pivot_row < ROW; ++pivot_row)
+		for (int pivot_col = 0; pivot_col < COL; ++pivot_col)
+			for (int i = pivot_row; i < ROW; ++i)
+				for (int j = pivot_col; j < COL; ++j)
+				{
+					// for every sub matrices in the matrix, find the sum of each sub matrices
+					int sum = 0;
+					for (int r = pivot_row; r <= i; ++r)
+						for (int s = pivot_col; s <= j; ++s)
+							sum += matrix[r*COL + s];
+					if (sum > max) {
+						sub_row1 = pivot_row;
+						sub_row2 = i;
+						sub_col1 = pivot_col;
+						sub_col2 = j;
+						max = sum;
+					}
+				}
+	cout << "maxSum:" << max << endl;
+	return max;
+}
+
+int MaxSum(const int* matrix, const int ROW, const int COL, int& sub_row1, int& sub_row2, int& sub_col1, int& sub_col2)
 {
 	int global_max = INT8_MIN;
     for (int l_col = 0; l_col < COL; ++l_col)
@@ -72,12 +99,26 @@ void MakeMatrix(int* matrix, int ROW, int COL)
 
 int main()
 {
-	int matrix[3][4] = { {-100,2,3,4} ,{5,100,7,8}, {-50,10,11,12} };
-	ShowMatrix(*matrix, 3, 4, 0, 0, 0, 0);
+    int ROW, COL;
+    cout << "please enter ROW and COL: ";
+    cin >> ROW >> COL;
+
+	int *matrix = new int[COL*ROW];
+    MakeMatrix(matrix, ROW, COL);
+	ShowMatrix(matrix, ROW, COL, 0, 0, 0, 0);
 	int sub_m1 = 0; 
 	int sub_m2 = 0;
 	int sub_n1 = 0;
 	int sub_n2 = 0;
-	MaxSum(*matrix, 3, 4, sub_m1, sub_m2, sub_n1, sub_n2);
-	ShowMatrix(*matrix, 3, 4, sub_m1, sub_m2, sub_n1, sub_n2);
+
+    MyUsage usage;
+    cout << "kadanes's algorithm: " << endl;
+	MaxSum(matrix, ROW, COL, sub_m1, sub_m2, sub_n1, sub_n2);
+	ShowMatrix(matrix, ROW, COL, sub_m1, sub_m2, sub_n1, sub_n2);
+    usage.report(true, true);
+
+    cout << "brute-force algorithm: " << endl;
+    MaxSum_brute(matrix, ROW, COL, sub_m1, sub_m2, sub_n1, sub_n2);
+	ShowMatrix(matrix, ROW, COL, sub_m1, sub_m2, sub_n1, sub_n2);
+    usage.report(true, true);
 }
